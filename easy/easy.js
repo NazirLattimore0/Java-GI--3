95459eb35a1d08cfe0941980c8fe74de1afad3a4
NazirLattimore0 <nlattim1@email.cpcc.edu>
2022-09-28 17:43:09 -0400

    second commit

--- easy/easy.js
+++ easy/easy.js
@@ -1,7 +1,7 @@
-var run = exercise("running");
-console.log(run()); // prints "Today's exercise: running"
-var swim = exercise("swimming");
-console.log(swim()); // prints "Today's exercise: swimming"
+// var run = exercise("running");
+// console.log(run()); // prints "Today's exercise: running"
+// var swim = exercise("swimming");
+// console.log(swim()); // prints "Today's exercise: swimming"
 
 var run = exercise("running");
 
